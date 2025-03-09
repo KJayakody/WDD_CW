fd1c4e8d6c0118ef8410c64f1c2306eb4fa937b1
maigarai <migaram@i360.lk>
2025-03-09 20:40:08 +0530

    Added Validation, Editor pages. Fixed Team and Feedback

--- scripts/team.script.js
+++ scripts/team.script.js
@@ -1,22 +1,24 @@
-/*TODO
- * Check stopPropagation()
- */
-
 const cards = document.querySelectorAll(".card");
 const cardContainer = document.querySelector(".card-container");
 
+// function for selecting a card. If a card is selected, it will be unselected and vice versa
 function cardSelection(card, event) {
   cards.forEach((c) => c.classList.add("not-selected"));
   card.classList.toggle("not-selected");
   card.classList.toggle("selected");
+  // Prevents the event from bubbling up the DOM tree, preventing any parent handlers from being notified of the event. | https://www.w3schools.com/jsref/event_stoppropagation.asp
   event.stopPropagation();
 }
 
+// Event listeners for each card
 cards.forEach((card) => {
+  // Event listener for clicking on a card
   card.addEventListener("click", (event) => {
     resetSelection();
     cardSelection(card, event);
   });
+
+  // Event listener for pressing enter on a card
   card.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
       cardSelection(card, event);
@@ -24,6 +26,7 @@ cards.forEach((card) => {
   });
 });
 
+// Function for resetting the selection of all cards
 function resetSelection() {
   cards.forEach((c) => {
     c.classList.remove("not-selected");
@@ -31,6 +34,7 @@ function resetSelection() {
   });
 }
 
+// Event listener for clicking outside of the card container
 document.addEventListener("click", (event) => {
   if (!cardContainer.contains(event.target)) {
     resetSelection();
